329768c55bb897c63a745be21a37b1bdc09c2af0
SilverEzredes <68148231+SilverEzredes@users.noreply.github.com>
2022-09-27 21:31:52 +0200

    Update Welcome Message

--- main.py
+++ main.py
@@ -212,10 +212,11 @@ async def main():
             await channel.send(content=f"<@!{member.id}>",
                                embed=utils.custom_embed(member.guild,
                                                         title="👋 Welcome!",
-                                                        description=f"Welcome <@!{member.id}> to Night City!\n"
+                                                        description=f"Welcome <@!{member.id}> to Uncharted Modding!\n"
                                                                     "\n" +
                                                                     (f"Make sure you have read through <#{globals.JOIN_LOG_CHANNEL_IDS[str(member.guild.id)]['rules_channel_id']}>!\n" if globals.JOIN_LOG_CHANNEL_IDS[str(member.guild.id)]["rules_channel_id"] else "") +
                                                                     (f"You can pick your poisons in <#{globals.JOIN_LOG_CHANNEL_IDS[str(member.guild.id)]['selfrole_channel_id']}>!\n" if globals.JOIN_LOG_CHANNEL_IDS[str(member.guild.id)]["selfrole_channel_id"] else "") +
+                                                                    "\n" +
                                                                     "Enjoy your stay!",
                                                         thumbnail=member.display_avatar.url))
 
